72b4decc9c9eaea45a8249d230333f91b04a79b3
a-shekhar <rajadityasingh31@gmail.com>
2024-09-24 23:24:15 +0530

    Generic Exception Handling

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springboot/restcontroller/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springboot/restcontroller/rest/StudentRestController.java
@@ -2,9 +2,7 @@ package com.springboot.restcontroller.rest;
 
 import com.springboot.restcontroller.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.HttpStatus;
 
 
 import java.util.ArrayList;
@@ -43,30 +41,5 @@ public class StudentRestController {
         return theStudents.get(studentId);
     }
 
-    // Add an Exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 
 }
