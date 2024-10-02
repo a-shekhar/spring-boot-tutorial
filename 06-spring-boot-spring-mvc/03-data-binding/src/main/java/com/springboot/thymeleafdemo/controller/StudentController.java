19dfc0fef5a9caa00655716b5aa033127cd7ad36
a-shekhar <rajadityasingh31@gmail.com>
2024-10-02 19:44:49 +0530

    Reading value from application.properties

--- 06-spring-boot-spring-mvc/03-data-binding/src/main/java/com/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/03-data-binding/src/main/java/com/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.springboot.thymeleafdemo.controller;
 
 import com.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     // method to show form for adding new student
@@ -19,6 +25,11 @@ public class StudentController {
 
         // add the student object to the model
         theModel.addAttribute("student", theStudent);
+
+        //  add the list of countries to the model
+        theModel.addAttribute("countries", countries);
+
+        // return the view name
 
         return "student-form";
     }
