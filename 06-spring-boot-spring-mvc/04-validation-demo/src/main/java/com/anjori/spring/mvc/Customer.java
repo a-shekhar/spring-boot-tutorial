e34dd42550c6dd4b4c18fe1935bafb5f814acf99
a-shekhar <rajadityashekhar@outlook.com>
2025-08-06 14:53:24 +0530

    Spring MVC validation

---
+++ 06-spring-boot-spring-mvc/04-validation-demo/src/main/java/com/anjori/spring/mvc/Customer.java
@@ -0,0 +1,21 @@
+package com.anjori.spring.mvc;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    private String firstName;
+
+    @NotNull(message= "is required")
+    @Size(min=1, message= "is required")
+    private String lastName;
+
+    @Min(value=0, message="Must be greater than or equal to Zero")
+    @Max(value=10, message="Must be less than or equal to Ten")
+    private int freePasses;
+}
