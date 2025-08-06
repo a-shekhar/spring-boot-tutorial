e1fd99016c332e9d2eedf647659874474f4f648b
a-shekhar <rajadityashekhar@outlook.com>
2025-08-06 15:04:04 +0530

    Regex validation

--- 06-spring-boot-spring-mvc/04-validation-demo/src/main/java/com/anjori/spring/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/04-validation-demo/src/main/java/com/anjori/spring/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.anjori.spring.mvc;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -18,4 +19,7 @@ public class Customer {
     @Min(value=0, message="Must be greater than or equal to Zero")
     @Max(value=10, message="Must be less than or equal to Ten")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message= "Only 5 chars/digits")
+    private String postalCode;
 }
