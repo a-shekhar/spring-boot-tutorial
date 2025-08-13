2d1f7b360d7774aa09660d073fab8e2ccab188a6
a-shekhar <rajadityashekhar@outlook.com>
2025-08-13 13:11:53 +0530

    Roles and URL based access and Custom Denied Page

---
+++ 08-spring-boot-spring-mvc-security/02-spring-boot-spring-mvc-security-roles/src/main/java/com/anjori/security/SecurityRolesApplication.java
@@ -0,0 +1,13 @@
+package com.anjori.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityRolesApplication.class, args);
+	}
+
+}
