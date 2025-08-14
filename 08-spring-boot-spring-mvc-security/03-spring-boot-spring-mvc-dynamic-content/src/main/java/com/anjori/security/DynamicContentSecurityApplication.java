6e5a4e0149a3921dbf238158aa8bf0bf18ebb8c1
a-shekhar <rajadityashekhar@outlook.com>
2025-08-14 13:30:03 +0530

    Dynamic Content Based on Roles

---
+++ 08-spring-boot-spring-mvc-security/03-spring-boot-spring-mvc-dynamic-content/src/main/java/com/anjori/security/DynamicContentSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.anjori.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicContentSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicContentSecurityApplication.class, args);
+	}
+
+}
